4bbaf7979c318111f876f1221c5d309da90ef925
mikk066h <mikk066h@student.eadania.dk>
2019-03-06 10:39:07 +0100

    Update Collision.cs

--- TheRatQueen/Componts/Collision.cs
+++ TheRatQueen/Componts/Collision.cs
@@ -8,57 +8,91 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace TheRatQueen
+{
+    public class Collision : Componet
+
     {
-        public  class Collision : Componet
+        public SpriteRendere spriteRendere { get; private set; }
 
+
+
+        public bool DoCollisionsChecks { get; set; }
+
+        private Texture2D texture;
+
+        public HashSet<Collision> otherColliders = new HashSet<Collision>();
+
+        public Rectangle CollisionBox
         {
-            public SpriteRendere spriteRendere { get; private set; }
-
-
-
-            public bool DoCollisionsChecks { get; set; }
-
-            private Texture2D texture;
-
-            public HashSet<Collision> otherColliders = new HashSet<Collision>();
-
-            public Rectangle CollisionBox
+            get
             {
-                get
+                return new Rectangle
+                    (
+                        (int)(GameObject.transform.Position.X - spriteRendere.orgin.X),
+                        (int)(GameObject.transform.Position.Y - spriteRendere.orgin.Y),
+                        spriteRendere.rectangle.Width,
+                        spriteRendere.rectangle.Height
+                    );
+            }
+        }
+
+        public override void Attach(GameObject gameObject)
+        {
+            base.Attach(gameObject);
+
+            DoCollisionsChecks = true;
+
+            spriteRendere = (SpriteRendere)gameObject.GetComponet("SpriteRendere");
+
+            GameWorld.Instance.Colliders.Add(this);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("CollisionTexture");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            CheckCollisions();
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle topLine = new Rectangle(CollisionBox.X, CollisionBox.Y, CollisionBox.Width, 1);
+            Rectangle bottomLine = new Rectangle(CollisionBox.X, CollisionBox.Y + CollisionBox.Height, CollisionBox.Width, 1);
+            Rectangle rightLine = new Rectangle(CollisionBox.X + CollisionBox.Width, CollisionBox.Y, 1, CollisionBox.Height);
+            Rectangle leftLine = new Rectangle(CollisionBox.X, CollisionBox.Y, 1, CollisionBox.Height);
+
+            spriteBatch.Draw(texture, topLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, bottomLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, rightLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, leftLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+        }
+        private void CheckCollisions()
+        {
+            if (DoCollisionsChecks)
+            {
+                foreach (Collision other in GameWorld.Instance.Colliders)
                 {
-                    return new Rectangle
-                        (
-                            (int)(GameObject.transform.Position.X - spriteRendere.orgin.X),
-                            (int)(GameObject.transform.Position.Y - spriteRendere.orgin.Y),
-                            spriteRendere.rectangle.Width,
-                            spriteRendere.rectangle.Height
-                        );
+                    if (other != this)
+                    {
+                        if (CollisionBox.Intersects(other.CollisionBox))
+                        {
+
+
+                            if (!otherColliders.Contains(other))
+                            {
+                                otherColliders.Add(other);
+                            }
+                        }
+                        else if ((otherColliders.Contains(other)))
+                        {
+                            otherColliders.Remove(other);
+                        }
+                    }
                 }
             }
-
-            public override void Attach(GameObject gameObject)
-            {
-                base.Attach(gameObject);
-
-                DoCollisionsChecks = true;
-
-                spriteRendere = (SpriteRendere)gameObject.GetComponet("SpriteRendere");
-
-                GameWorld.Instance.Colliders.Add(this);
-            }
-
-            public override void LoadContent(ContentManager content)
-            {
-                texture = content.Load<Texture2D>("CollisionTexture");
-            }
-
-            public override void Update(GameTime gameTime)
-            {
-                CheckColliders();
-            }
-
-
-
         }
     }
+}
 
